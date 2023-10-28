a9805698fdf4fb0d02e88e4bf99004e10c70a6fa
Bhavsachi Kaur <143928732+bhavsachi24@users.noreply.github.com>
2023-10-28 22:52:27 +0530

    Add files via upload

---
+++ Task1_Landing page/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("a").on("click", function (event) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        } 
+    });
+});
